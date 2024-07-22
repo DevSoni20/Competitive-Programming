1ce2ba2da8321cbdbbc2af8cc97805bd4a4d1f0f
devsoni-20 <devsoni4545@gmail.com>
2024-07-22 15:22:33 +0530

    adding Constructive folder with Bit++ Solution

---
+++ Constructive/Bit+.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int val = 0;
+    while(n--)
+    {
+        string s;
+        cin>>s;
+        if(s[1] == '+'){val++;}
+        else{val--;}
+    }
+    cout<<val<<endl;
+}
